1f79ba8b31ca8c4645f9810ac540a3c6e96fc8e6
lecthh <dev.lecth@gmail.com>
2023-12-17 04:13:34 +0800

    asffasf

--- resources/views/modals/college/edit_college.blade.php
+++ resources/views/modals/college/edit_college.blade.php
@@ -1,5 +1,5 @@
 <div id="edit-college" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-    <form action="{{ url('colleges/' .$item->collid) }}" method="POST">
+    <form action="{{ url('colleges/' .$college->collid) }}" method="post">
         {{ csrf_field() }}
         {{ method_field('PATCH') }}
         <div class="w-[500px] bg-white flex flex-col gap-y-6 p-6">
